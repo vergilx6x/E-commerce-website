e40e7980c7d74954d2a3c2ec606ab387058b8a1a
Mohamed Amine Thami <136102816+vergilx6x@users.noreply.github.com>
2024-09-10 00:11:24 +0100

    Update db_engine.py

--- models/engine/db_engine.py
+++ models/engine/db_engine.py
@@ -24,7 +24,7 @@ class DBStorage:
     def __init__(self):
 
         mysql_user = "admin"
-        mysql_pwd = "HHesoyaMMX6XX.."
+        mysql_pwd = ""
         mysql_host = "localhost"
         mysql_db = "website_db"
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
